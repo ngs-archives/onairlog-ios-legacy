da4572d787e18e3e6429743a9c5ec7dd047c1579
Atsushi Nagase <a@ngs.io>
2014-10-12 00:40:15 +0900

    Fix codesign

--- Scripts/install-mobileprovisioning.sh
+++ Scripts/install-mobileprovisioning.sh
@@ -2,7 +2,7 @@
 set -eu
 BASE=~/Library/MobileDevice/Provisioning\ Profiles
 mkdir -p "$BASE"
-for file in MobileProvisionings/*.*provision*; do
+for file in MobileProvisionings/*$PROVISIONING_SUFFIX.*provision*; do
   uuid=`grep UUID -A1 -a "$file" | grep -io "[-A-Z0-9]\{36\}"`
   extension="${file##*.}"
   echo "$file -> $uuid"
