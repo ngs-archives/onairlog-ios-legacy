a3e12528355146b5d08900bdfd731a37803dbe6f
Atsushi Nagase <a@ngs.io>
2015-03-13 01:55:42 +0900

    Refresh button

--- NowOnAirWatchKitExtension/InterfaceController.swift
+++ NowOnAirWatchKitExtension/InterfaceController.swift
@@ -13,47 +13,58 @@ import Foundation
 class InterfaceController: WKInterfaceController {
   var song: Song? = nil
   var apiClient: SongAPIClient? = nil
-
+  
   @IBOutlet weak var timestampLabel: WKInterfaceLabel!
   @IBOutlet weak var titleLabel: WKInterfaceLabel!
   @IBOutlet weak var artistLabel: WKInterfaceLabel!
-    override func awakeWithContext(context: AnyObject?) {
-      super.awakeWithContext(context)
-      let dbURL = NSFileManager.defaultManager()
-        .containerURLForSecurityApplicationGroupIdentifier(kOnAirLogDocumentContainerDomain)?
-        .URLByAppendingPathComponent("OnAirLog.sqlite")
-      MagicalRecord.setupCoreDataStackWithStoreAtURL(dbURL)
-      self.apiClient = SongAPIClient()
-      // Google Analytics
-      let gai = GAI.sharedInstance()
-      gai.trackUncaughtExceptions = true
-      gai.dispatchInterval = 20
-      gai.trackerWithTrackingId(kOnAirLogGATrackingId)
-      self.updateSong()
-    }
-
-    override func willActivate() {
-      super.willActivate()
-      let tracker = GAI.sharedInstance().defaultTracker
-      tracker.set(kGAIScreenName, value: "Today Widget")
-      tracker.send(GAIDictionaryBuilder.createAppView().build())
-      self.apiClient?.load(0,
-        success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
-          if self.updateSong() {
-            tracker.send(GAIDictionaryBuilder.createEventWithCategory("widget", action: "new-data", label: self.song?.songID.stringValue, value: 1).build())
-          } else {
-            tracker.send(GAIDictionaryBuilder.createEventWithCategory("widget", action: "no-data", label: self.song?.songID.stringValue, value: 1).build())
-          }
-        },
-        failure: { (task: NSURLSessionDataTask!,  error: NSError!) -> Void in
-          tracker.send(GAIDictionaryBuilder.createEventWithCategory("widget", action: "failed", label: self.song?.songID.stringValue, value: 1).build())
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    let dbURL = NSFileManager.defaultManager()
+      .containerURLForSecurityApplicationGroupIdentifier(kOnAirLogDocumentContainerDomain)?
+      .URLByAppendingPathComponent("OnAirLog.sqlite")
+    MagicalRecord.setupCoreDataStackWithStoreAtURL(dbURL)
+    self.apiClient = SongAPIClient()
+    // Google Analytics
+    let gai = GAI.sharedInstance()
+    gai.trackUncaughtExceptions = true
+    gai.dispatchInterval = 20
+    gai.trackerWithTrackingId(kOnAirLogGATrackingId)
+    self.updateSong()
+  }
+  
+  override func willActivate() {
+    super.willActivate()
+    let tracker = GAI.sharedInstance().defaultTracker
+    tracker.set(kGAIScreenName, value: "Watch App")
+    tracker.send(GAIDictionaryBuilder.createAppView().build())
+    self.refresh()
+  }
+  
+  @IBAction func refreshAction(sender: WKInterfaceButton) {
+    let tracker = GAI.sharedInstance().defaultTracker
+    tracker.send(GAIDictionaryBuilder.createEventWithCategory("watch", action: "refresh", label: self.song?.songID.stringValue, value: 1).build())
+    self.refresh()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  func refresh() {
+    let tracker = GAI.sharedInstance().defaultTracker
+    self.apiClient?.load(0,
+      success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+        if self.updateSong() {
+          tracker.send(GAIDictionaryBuilder.createEventWithCategory("watch", action: "new-data", label: self.song?.songID.stringValue, value: 1).build())
+        } else {
+          tracker.send(GAIDictionaryBuilder.createEventWithCategory("watch", action: "no-data", label: self.song?.songID.stringValue, value: 1).build())
         }
-      )
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+      },
+      failure: { (task: NSURLSessionDataTask!,  error: NSError!) -> Void in
+        tracker.send(GAIDictionaryBuilder.createEventWithCategory("watch", action: "failed", label: self.song?.songID.stringValue, value: 1).build())
+      }
+    )
   }
   
   func updateSong() -> Bool {
@@ -73,5 +84,5 @@ class InterfaceController: WKInterfaceController {
     self.timestampLabel.setText("")
     return false
   }
-
+  
 }
