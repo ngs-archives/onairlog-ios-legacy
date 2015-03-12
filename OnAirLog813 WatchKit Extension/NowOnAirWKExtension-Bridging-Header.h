24debd43e713a5b5cdec7e643d9e425b4e198aa0
Atsushi Nagase <a@ngs.io>
2015-03-12 23:44:41 +0900

    Works with 813

---
+++ OnAirLog813 WatchKit Extension/NowOnAirWKExtension-Bridging-Header.h
@@ -0,0 +1,11 @@
+#import <AFNetworking/AFNetworking.h>
+#import <BlocksKit/BlocksKit.h>
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
+#import <GoogleAnalytics-iOS-SDK/GAI.h>
+#import <GoogleAnalytics-iOS-SDK/GAIFields.h>
+#import <GoogleAnalytics-iOS-SDK/GAIDictionaryBuilder.h>
+#define MR_SHORTHAND
+#import <MagicalRecord/MagicalRecord.h>
+#import <MagicalRecord/CoreData+MagicalRecord.h>
+#import "Song.h"
